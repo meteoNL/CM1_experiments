correct MSE calculation error

cm1r19.8/run/scripts/MSE_cross.py
@@ -50,7 +50,7 @@ for i in np.arange(steps):
     pl.text((lensim*0.52),10, fullname, verticalalignment='center',rotation=90)
     
     #save and add filenames to list for gif
-    fn = str(path+namesim+"/"+name_figs+"%.3d" % i+".png")
+    fn = str(path+namesim+"/pngs/"+name_figs+"%.3d" % i+".png")
     pl.savefig(fn)
     fnames+=[fn]
 #%%

cm1r19.8/run/scripts/preparation_script.py
@@ -14,7 +14,7 @@ import matplotlib
 matplotlib.rcParams.update({'font.size': 18}) #larger font in the plots
 
 path="/lustre/project/m2_jgu-w2w/w2w/egroot/CM1mod/cm1r19.8/run/"
-namesim = "controlling_MSEadv_0.995"; ccf = 5.000 #specify name of simulation and its factor to get the correct cross-section at constant x
+namesim = "controlling_lve_1.2"; ccf = 5.000 #specify name of simulation and its factor to get the correct cross-section at constant x
 varname = "w" #variable of which the top view for level given below is plotted with read_nc_CM1.py
 lvl=115 #level at which we will look if appropriate
 name_figs = "simulation_"+varname
@@ -34,8 +34,8 @@ minimum,maximum = autoextremes(test[varname])
 clrs = np.linspace(minimum, maximum)
 #define time axis
 steps = len(test["time"])
-time = np.linspace(0,lensim,steps)
+time = test["time"][:]
 
 #calculate divergence and instantaneous moist static energy distribution
 div=D2div(test,xmask,ymask)
-MSE=MSE_inst(test,1.0)
+MSE=MSE_inst(test,1.2)

cm1r19.8/run/scripts/scripts_per_T/preparation_script.py
@@ -33,7 +33,7 @@ minimum,maximum = autoextremes(test[varname])
 clrs = np.linspace(minimum, maximum)
 #define time axis
 steps = len(test["time"])
-time = np.linspace(0,lensim,steps)
+time = test["time"][:]
 
 #calculate divergence and instantaneous moist static energy distribution
 div=D2div(test,xmask,ymask)